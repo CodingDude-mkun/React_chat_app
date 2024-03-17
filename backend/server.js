b7bad6b463a1f252525fa6e5ce1c9071677158b3
CodingDude-mkun <pathadem019@gmail.com>
2024-03-17 23:35:11 +0530

    Updated build scripts

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import dotenv from "dotenv";
 import express from "express";
 import cookieParser from "cookie-parser";
+import { path } from "path";
+
 import authRoute from "./Routes/auth.routes.js";
 import userRoutes from "./Routes/user.routes.js";
 import messageRoutes from "./Routes/message.routes.js";
@@ -8,6 +10,8 @@ import connectToMongoDB from "./DB/connectToMondoDB.js";
 import { app, server } from "./socket/socket.js";
 
 const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
 
 dotenv.config();
 
@@ -18,9 +22,10 @@ app.use("/api/auth", authRoute);
 app.use("/api/message", messageRoutes);
 app.use("/api/users", userRoutes);
 
-// app.get("/", (req, res) => {
-//   res.send("Hello World!");
-// });
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(PORT, () => {
   connectToMongoDB();
